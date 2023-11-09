585d982539416bc67c92006a7bf889075abc874b
hwk <hwk1727370054@126.com>
2023-11-09 11:02:03 +0800

    fix problem

--- test.cc
+++ test.cc
@@ -4,6 +4,6 @@ int main()
 {
     std::cout << "在master分支下" << std::endl;
     std::cout << "在b1分支下" << std::endl;
-    std::cout << "hello world" << std::endl;
+    std::cout << "修复了master branch的功能" << std::endl;
     return 0;
 }
